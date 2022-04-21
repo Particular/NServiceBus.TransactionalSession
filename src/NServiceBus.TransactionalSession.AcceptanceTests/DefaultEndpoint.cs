115e2ab407fbda118700df86e6990a30e3ea5cb6
Tomek Masternak <tomek.masternak@gmail.com>
2022-04-21 14:59:21 +0200

    att setup

---
+++ src/NServiceBus.TransactionalSession.AcceptanceTests/DefaultEndpoint.cs
@@ -0,0 +1,46 @@
+﻿namespace NServiceBus.TransactionalSession.AcceptanceTests.EndpointTemplates
+{
+    using System;
+    using System.Threading.Tasks;
+    using AcceptanceTesting.Customization;
+    using AcceptanceTesting.Support;
+    using Microsoft.Extensions.DependencyInjection;
+    using NServiceBus;
+
+    class DefaultEndpoint : IEndpointSetupTemplate
+    {
+#pragma warning disable PS0018 // A task-returning method should have a CancellationToken parameter unless it has a parameter implementing ICancellableContext
+        public async Task<EndpointConfiguration> GetConfiguration(
+#pragma warning restore PS0018 // A task-returning method should have a CancellationToken parameter unless it has a parameter implementing ICancellableContext
+            RunDescriptor runDescriptor,
+            EndpointCustomizationConfiguration endpointConfiguration,
+#pragma warning disable PS0013 // A Func used as a method parameter with a Task, ValueTask, or ValueTask<T> return type argument should have at least one CancellationToken parameter type argument unless it has a parameter type argument implementing ICancellableContext
+            Func<EndpointConfiguration, Task> configurationBuilderCustomization)
+#pragma warning restore PS0013 // A Func used as a method parameter with a Task, ValueTask, or ValueTask<T> return type argument should have at least one CancellationToken parameter type argument unless it has a parameter type argument implementing ICancellableContext
+        {
+            var configuration = new EndpointConfiguration(endpointConfiguration.EndpointName);
+
+            configuration.TypesToIncludeInScan(endpointConfiguration.GetTypesScopedByTestClass());
+            configuration.EnableInstallers();
+
+            configuration.RegisterComponents(c => c
+                .AddSingleton(runDescriptor.ScenarioContext.GetType(), runDescriptor.ScenarioContext));
+
+            var recoverability = configuration.Recoverability();
+            recoverability.Delayed(delayed => delayed.NumberOfRetries(0));
+            recoverability.Immediate(immediate => immediate.NumberOfRetries(0));
+            configuration.SendFailedMessagesTo("error");
+
+            configuration.UseTransport(new LearningTransport
+            {
+                TransportTransactionMode = TransportTransactionMode.ReceiveOnly
+            });
+
+            configuration.UsePersistence<AcceptanceTestingPersistence>();
+
+            await configurationBuilderCustomization(configuration).ConfigureAwait(false);
+
+            return configuration;
+        }
+    }
+}
---
+++ src/NServiceBus.TransactionalSession.AcceptanceTests/When_running_outbox.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using NServiceBus;
+using NServiceBus.AcceptanceTesting;
+using NServiceBus.Features;
+using NServiceBus.TransactionalSession;
+using NServiceBus.TransactionalSession.AcceptanceTests.EndpointTemplates;
+using NUnit.Framework;
+
+public class When_running_outbox
+{
+    [Test]
+    public async Task Should_send_messages_on_transactional_session_commit()
+    {
+        await Scenario.Define<Context>()
+            .WithEndpoint<AnEndpoint>(s => s.When((ms, ctx) =>
+            {
+                ctx.Started = true;
+                return Task.CompletedTask;
+            }))
+            .Done(c => c.MessageReceived)
+            .Run()
+            .ConfigureAwait(false);
+    }
+
+    class Context : ScenarioContext
+    {
+        public bool MessageReceived { get; set; }
+        public bool Started { get; set; }
+    }
+
+    class AnEndpoint : EndpointConfigurationBuilder
+    {
+        public AnEndpoint()
+        {
+            EndpointSetup<DefaultEndpoint>((c, r) =>
+            {
+                c.EnableOutbox();
+
+                c.EnableFeature<TransactionalSessionFeature>();
+
+                c.RegisterStartupTask(sp => new SendMessageViaTransactionalSession(sp, r.ScenarioContext as Context));
+            });
+        }
+
+        class SendMessageViaTransactionalSession : FeatureStartupTask
+        {
+            readonly IServiceProvider serviceProvider;
+            readonly Context context;
+
+            public SendMessageViaTransactionalSession(IServiceProvider serviceProvider, Context context)
+            {
+                this.serviceProvider = serviceProvider;
+                this.context = context;
+            }
+
+            protected override Task OnStart(IMessageSession session, CancellationToken cancellationToken = default)
+            {
+                _ = Task.Run(async () =>
+                {
+                    while (context.Started == false)
+                    {
+                        await Task.Delay(TimeSpan.FromMilliseconds(50)).ConfigureAwait(false);
+                    }
+
+                    using (var scope = serviceProvider.CreateScope())
+                    {
+                        var transactionalSession = scope.ServiceProvider.GetRequiredService<ITransactionalSession>();
+
+                        await transactionalSession.Open(cancellationToken).ConfigureAwait(false);
+
+                        await transactionalSession.SendLocal(new SampleMessage(), cancellationToken)
+                            .ConfigureAwait(false);
+
+                        await transactionalSession.Commit(cancellationToken).ConfigureAwait(false);
+                    }
+                }, cancellationToken).ConfigureAwait(false);
+
+                return Task.CompletedTask;
+            }
+
+            protected override Task OnStop(IMessageSession session, CancellationToken cancellationToken = default) => Task.CompletedTask;
+        }
+    }
+
+    class SampleMessage : ICommand
+    {
+    }
+}
