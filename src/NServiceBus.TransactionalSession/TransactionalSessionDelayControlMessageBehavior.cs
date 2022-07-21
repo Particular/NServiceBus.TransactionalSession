ca958dc044d72f6641c0cfb30b3c51cee55c8320
Daniel Marbach <daniel.marbach@openplace.net>
2022-07-21 12:03:18 +0200

    Adjust behavior to the diagram

--- src/NServiceBus.TransactionalSession/TransactionalSessionDelayControlMessageBehavior.cs
+++ src/NServiceBus.TransactionalSession/TransactionalSessionDelayControlMessageBehavior.cs
@@ -3,25 +3,21 @@ namespace NServiceBus.TransactionalSession
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Threading;
     using System.Threading.Tasks;
     using Logging;
-    using Outbox;
     using Pipeline;
     using Routing;
     using Transport;
 
     class TransactionalSessionDelayControlMessageBehavior : Behavior<IIncomingPhysicalMessageContext>
     {
-        readonly IOutboxStorage outboxStorage;
         readonly IMessageDispatcher dispatcher;
         readonly string physicalQueueAddress;
         TimeSpan MaxCommitDelay = TimeSpan.FromSeconds(15);
         TimeSpan CommitDelayIncrement = TimeSpan.FromSeconds(5);
 
-        public TransactionalSessionDelayControlMessageBehavior(IOutboxStorage outboxStorage, IMessageDispatcher dispatcher, string physicalQueueAddress)
+        public TransactionalSessionDelayControlMessageBehavior(IMessageDispatcher dispatcher, string physicalQueueAddress)
         {
-            this.outboxStorage = outboxStorage;
             this.dispatcher = dispatcher;
             this.physicalQueueAddress = physicalQueueAddress;
         }
@@ -39,22 +35,25 @@ namespace NServiceBus.TransactionalSession
             var commitStartedAtText = context.Message.Headers[OutboxTransactionalSession.ControlMessageSentAtHeaderName];
             var commitStartedAt = DateTimeOffsetHelper.ToDateTimeOffset(commitStartedAtText);
 
-            var messageId = context.MessageId;
-
-            var outboxRecord = await outboxStorage.Get(messageId, context.Extensions, CancellationToken.None).ConfigureAwait(false);
-            var transactionCommitted = outboxRecord != null;
             var timeSinceCommitStart = DateTimeOffset.UtcNow.Subtract(commitStartedAt);
 
-            if (transactionCommitted || timeSinceCommitStart > MaxCommitDelay)
+            var messageId = context.MessageId;
+            if (timeSinceCommitStart > MaxCommitDelay)
             {
-                await next().ConfigureAwait(false);
+                if (Log.IsInfoEnabled)
+                {
+                    Log.Info($"Consuming transaction commit control messages for messageId={messageId} to create the outbox tomb stone.");
+                }
                 return;
             }
 
-            Log.Debug($"Delaying transaction commit control messages for messageId={messageId}");
+            if (Log.IsDebugEnabled)
+            {
+                Log.Debug($"Delaying transaction commit control messages for messageId={messageId}");
+            }
 
             await dispatcher.Dispatch(new TransportOperations(
-                    new Transport.TransportOperation(
+                    new TransportOperation(
                         new OutgoingMessage(messageId, context.MessageHeaders.ToDictionary(kvp => kvp.Key, kvp => kvp.Value), ReadOnlyMemory<byte>.Empty),
                         new UnicastAddressTag(physicalQueueAddress),
                         new DispatchProperties(new Dictionary<string, string>
@@ -69,7 +68,6 @@ namespace NServiceBus.TransactionalSession
             throw new ConsumeMessageException();
         }
 
-        static ILog Log = LogManager.GetLogger<TransactionalSessionDelayControlMessageBehavior>();
-
+        static readonly ILog Log = LogManager.GetLogger<TransactionalSessionDelayControlMessageBehavior>();
     }
 }
