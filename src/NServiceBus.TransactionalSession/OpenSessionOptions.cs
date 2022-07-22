f9f8219ab37156b9c12fc466fe45c6a71541323a
Daniel Marbach <daniel.marbach@openplace.net>
2022-07-22 14:14:05 +0200

    Documentation

--- src/NServiceBus.TransactionalSession/OpenSessionOptions.cs
+++ src/NServiceBus.TransactionalSession/OpenSessionOptions.cs
@@ -23,20 +23,18 @@ namespace NServiceBus.TransactionalSession
         internal bool HasExtensions => extensions != null;
 
         /// <summary>
-        /// Session metadata.
+        /// Session metadata that gets added during the session commit operation.
         /// </summary>
         public IDictionary<string, string> Metadata => metadata ??= new Dictionary<string, string>();
 
         internal bool HasMetadata => metadata != null;
 
         /// <summary>
-        /// TBD
+        /// The maximum duration the transaction is allowed to attempt to atomically commit.
         /// </summary>
+        /// <remarks>The actual total transaction time observed might be longer, taking into account delays in the transport due to latency, delayed delivery and more.</remarks>
         public TimeSpan MaximumCommitDuration { get; set; } = TimeSpan.FromSeconds(15);
 
-        /// <summary>
-        ///
-        /// </summary>
         internal TimeSpan CommitDelayIncrement { get; set; } = TimeSpan.FromSeconds(2);
     }
 }
