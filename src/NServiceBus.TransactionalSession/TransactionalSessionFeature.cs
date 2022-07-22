4a75e9ea8358ce03fbca75a287419c6b1371cbc9
Daniel Marbach <daniel.marbach@openplace.net>
2022-07-22 14:51:18 +0200

    The TODO is no longer relevant

--- src/NServiceBus.TransactionalSession/TransactionalSessionFeature.cs
+++ src/NServiceBus.TransactionalSession/TransactionalSessionFeature.cs
@@ -47,7 +47,6 @@
 
             if (isOutboxEnabled)
             {
-                //TODO: what happens when someone turns off the Outbox but control messages are still in the input queue?
                 context.Pipeline.Register(sp => new TransactionalSessionDelayControlMessageBehavior(sp.GetRequiredService<IMessageDispatcher>(),
                     sp.GetRequiredService<ITransportAddressResolver>().ToTransportAddress(localQueueAddress)
                 ), "Transaction commit control message delay behavior");
