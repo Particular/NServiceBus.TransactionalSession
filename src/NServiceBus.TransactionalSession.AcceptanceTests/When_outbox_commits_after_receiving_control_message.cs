0a590c87632dd2e5b93d02aed48b21a21b90bf82
Tomek Masternak <tomasz.masternak@particular.net>
2022-07-28 12:14:20 +0200

    rest of acceptance tests

---
+++ src/NServiceBus.TransactionalSession.AcceptanceTests/When_outbox_commits_after_receiving_control_message.cs
@@ -0,0 +1,148 @@
+﻿namespace NServiceBus.TransactionalSession.AcceptanceTests
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using Features;
+    using NServiceBus.AcceptanceTests;
+    using NServiceBus.AcceptanceTests.EndpointTemplates;
+    using NUnit.Framework;
+    using ObjectBuilder;
+    using Pipeline;
+
+    public class When_outbox_commits_after_receiving_control_message : NServiceBusAcceptanceTest
+    {
+        [SetUp]
+        public void LoadAssemblies()
+        {
+            _ = typeof(ITransactionalSession).GetType();
+            _ = typeof(CustomTestingPersistence).GetType();
+        }
+
+        [Test]
+        public async Task Should_retry_till_outbox_transaction_commited()
+        {
+            await Scenario.Define<Context>()
+                .WithEndpoint<SenderEndpoint>(e => e
+                    .When(async (_, context) =>
+                    {
+                        using var scope = context.Builder.CreateChildBuilder();
+                        using var transactionalSession = scope.Build<ITransactionalSession>();
+
+                        var options = new OpenSessionOptions();
+                        options.Extensions.Set(CustomTestingOutboxTransaction.TransactionCommitTCSKey, context.TxCommitTcs = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously));
+
+                        await transactionalSession.Open(options);
+                        await transactionalSession.Send(new SomeMessage());
+                        await transactionalSession.Send(new SomeMessage());
+                        await transactionalSession.Send(new SomeMessage());
+
+                        await transactionalSession.Commit();
+                    }))
+                .WithEndpoint<ReceiverEndpoint>()
+                .Done(c => c.MessageReceiveCounter == 3)
+                .Run(TimeSpan.FromSeconds(15));
+        }
+
+        class Context : ScenarioContext, IInjectBuilder
+        {
+            public int MessageReceiveCounter = 0;
+            public IBuilder Builder { get; set; }
+            public TaskCompletionSource<bool> TxCommitTcs { get; set; }
+        }
+
+        class SenderEndpoint : EndpointConfigurationBuilder
+        {
+            public SenderEndpoint() =>
+                EndpointSetup<TransactionSessionWithOutboxEndpoint>((c, r) =>
+                {
+                    c.EnableTransactionalSession();
+                    c.EnableOutbox();
+
+                    c.EnableFeature<CaptureBuilderFeature>();
+
+                    var receiverEndpointName = AcceptanceTesting.Customization.Conventions.EndpointNamingConvention(typeof(ReceiverEndpoint));
+
+                    c.ConfigureTransport().Routing().RouteToEndpoint(typeof(SomeMessage), receiverEndpointName);
+
+                    c.Pipeline.Register(new DelayedOutboxTransactionCommitBehavior((Context)r.ScenarioContext), "delays the outbox transaction commit");
+                });
+
+
+            class DelayedOutboxTransactionCommitBehavior : Behavior<ITransportReceiveContext>
+            {
+                public DelayedOutboxTransactionCommitBehavior(Context testContext) => this.testContext = testContext;
+
+                public override async Task Invoke(ITransportReceiveContext context, Func<Task> next)
+                {
+                    try
+                    {
+                        await next();
+                    }
+                    catch (ConsumeMessageException)
+                    {
+                        // we don't know the order of behaviors
+                    }
+
+                    // unblock transaction once the receive pipeline "completed" once
+                    testContext.TxCommitTcs.TrySetResult(true);
+                }
+
+                readonly Context testContext;
+            }
+        }
+
+        class ReceiverEndpoint : EndpointConfigurationBuilder
+        {
+            public ReceiverEndpoint() => EndpointSetup<DefaultServer>();
+
+            class MessageHandler : IHandleMessages<SomeMessage>
+            {
+                Context testContext;
+
+                public MessageHandler(Context testContext) => this.testContext = testContext;
+
+
+                public Task Handle(SomeMessage message, IMessageHandlerContext context)
+                {
+                    Interlocked.Increment(ref testContext.MessageReceiveCounter);
+                    return Task.CompletedTask;
+                }
+            }
+        }
+
+        class SomeMessage : IMessage
+        {
+        }
+
+        public class CaptureBuilderFeature : Feature
+        {
+            protected override void Setup(FeatureConfigurationContext context)
+            {
+                var scenarioContext = context.Settings.Get<ScenarioContext>();
+                context.RegisterStartupTask(builder => new CaptureServiceProviderStartupTask(builder, scenarioContext));
+            }
+
+            class CaptureServiceProviderStartupTask : FeatureStartupTask
+            {
+                public CaptureServiceProviderStartupTask(IBuilder builder, ScenarioContext context)
+                {
+                    if (context is IInjectBuilder c)
+                    {
+                        c.Builder = builder;
+                    }
+                }
+
+                protected override Task OnStart(IMessageSession session) => Task.CompletedTask;
+
+                protected override Task OnStop(IMessageSession session) => Task.CompletedTask;
+            }
+        }
+
+        public interface IInjectBuilder
+        {
+            IBuilder Builder { get; set; }
+        }
+    }
+}
---
+++ src/NServiceBus.TransactionalSession.AcceptanceTests/When_outbox_entry_becomes_visible_after_tx_timeout.cs
@@ -0,0 +1,140 @@
+﻿namespace NServiceBus.TransactionalSession.AcceptanceTests
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Features;
+    using NServiceBus.AcceptanceTesting;
+    using NServiceBus.AcceptanceTests;
+    using NServiceBus.AcceptanceTests.EndpointTemplates;
+    using NServiceBus.Outbox;
+    using NUnit.Framework;
+    using ObjectBuilder;
+    using Pipeline;
+
+    public class When_outbox_entry_becomes_visible_after_tx_timeout : NServiceBusAcceptanceTest
+    {
+        [SetUp]
+        public void LoadAssemblies()
+        {
+            _ = typeof(ITransactionalSession).GetType();
+            _ = typeof(CustomTestingPersistence).GetType();
+        }
+        [Test]
+        public async Task Should_fail_to_process_control_message()
+        {
+            var context = await Scenario.Define<Context>()
+                .WithEndpoint<SenderEndpoint>(e => e
+                    .DoNotFailOnErrorMessages()
+                    .When(async (_, ctx) =>
+                    {
+                        using var scope = ctx.Builder.CreateChildBuilder();
+                        using var transactionalSession = scope.Build<ITransactionalSession>();
+
+                        var options = new OpenSessionOptions { MaximumCommitDuration = TimeSpan.Zero };
+                        await transactionalSession.Open(options);
+
+                        await transactionalSession.Send(new SomeMessage());
+
+                        await transactionalSession.Commit();
+
+                        ctx.TransactionalSessionId = transactionalSession.SessionId;
+                    }))
+                .WithEndpoint<ReceiverEndpoint>()
+                .Done(c => c.FailedMessages.Count > 0)
+                .Run(TimeSpan.FromSeconds(90));
+
+            Assert.IsFalse(context.MessageReceived, "message should never be dispatched");
+            var failedMessage = context.FailedMessages.Single().Value.Single();
+            // message should fail because it can't create an outbox record for the control message since the sender has already created the record and this causes a concurrency exception
+            // once the failed control message retries, the outbox record should be correctly found by the storage and the contained messages will be dispatched.
+            Assert.AreEqual($"Outbox message with id '{context.TransactionalSessionId}' is already present in storage.", failedMessage.Exception.Message);
+            Assert.AreEqual(context.TransactionalSessionId, failedMessage.MessageId);
+
+        }
+
+        class Context : ScenarioContext, IInjectBuilder
+        {
+            public IBuilder Builder { get; set; }
+            public bool MessageReceived { get; set; }
+            public string TransactionalSessionId { get; set; }
+        }
+
+        class SenderEndpoint : EndpointConfigurationBuilder
+        {
+            public SenderEndpoint() =>
+                EndpointSetup<DefaultServer>((c, r) =>
+                {
+                    c.EnableTransactionalSession();
+                    c.EnableOutbox();
+
+                    c.EnableFeature<CaptureBuilderFeature>();
+
+                    var receiverEndpointName = AcceptanceTesting.Customization.Conventions.EndpointNamingConvention(typeof(ReceiverEndpoint));
+                    c.ConfigureTransport().Routing().RouteToEndpoint(typeof(SomeMessage), receiverEndpointName);
+                    c.Pipeline.Register(new StorageManipulationBehavior(), "configures the outbox to not see the commited values yet");
+                });
+
+            class StorageManipulationBehavior : Behavior<ITransportReceiveContext>
+            {
+                public override Task Invoke(ITransportReceiveContext context, Func<Task> next)
+                {
+                    context.Extensions.Set<OutboxMessage>("TestOutboxStorage.GetResult", null); // no outbox record will be found
+
+                    return next();
+                }
+            }
+        }
+
+        class ReceiverEndpoint : EndpointConfigurationBuilder
+        {
+            public ReceiverEndpoint() => EndpointSetup<DefaultServer>();
+
+            class MessageHandler : IHandleMessages<SomeMessage>
+            {
+                public MessageHandler(Context testContext) => this.testContext = testContext;
+
+                public Task Handle(SomeMessage message, IMessageHandlerContext context)
+                {
+                    testContext.MessageReceived = true;
+                    return Task.CompletedTask;
+                }
+
+                Context testContext;
+            }
+        }
+
+        class SomeMessage : IMessage
+        {
+        }
+
+        public class CaptureBuilderFeature : Feature
+        {
+            protected override void Setup(FeatureConfigurationContext context)
+            {
+                var scenarioContext = context.Settings.Get<ScenarioContext>();
+                context.RegisterStartupTask(builder => new CaptureServiceProviderStartupTask(builder, scenarioContext));
+            }
+
+            class CaptureServiceProviderStartupTask : FeatureStartupTask
+            {
+                public CaptureServiceProviderStartupTask(IBuilder builder, ScenarioContext context)
+                {
+                    if (context is IInjectBuilder c)
+                    {
+                        c.Builder = builder;
+                    }
+                }
+
+                protected override Task OnStart(IMessageSession session) => Task.CompletedTask;
+
+                protected override Task OnStop(IMessageSession session) => Task.CompletedTask;
+            }
+        }
+
+        public interface IInjectBuilder
+        {
+            IBuilder Builder { get; set; }
+        }
+    }
+}
