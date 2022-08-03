6f8ee7149cb1e1135afe3c393cdd95b9ec5b7968
Tim Bussmann <timbussmann@users.noreply.github.com>
2022-08-03 18:45:02 +0200

    remove dependency on acceptancetesting sources package

---
+++ src/NServiceBus.TransactionalSession.AcceptanceTests/TestingPersistenceSetup.cs
@@ -0,0 +1,17 @@
+﻿using NServiceBus;
+using NServiceBus.AcceptanceTesting;
+using NServiceBus.TransactionalSession.AcceptanceTests;
+using NUnit.Framework;
+
+[SetUpFixture]
+public class TestingPersistenceSetup
+{
+    [OneTimeSetUp]
+    public void Setup()
+    {
+        TransactionSessionDefaultServer.ConfigurePersistence = configuration =>
+        {
+            var persistence = configuration.UsePersistence<CustomTestingPersistence>();
+        };
+    }
+}
