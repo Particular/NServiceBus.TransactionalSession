094b089a7b6ab330124bcfbc17ab7bd2ba9dd38d
Tim Bussmann <timbussmann@users.noreply.github.com>
2022-08-03 18:45:36 +0200

    add mongodb att

---
+++ src/NServiceBus.TransactionalSession.AcceptanceTests.MongoDB/MongoSetup.cs
@@ -0,0 +1,19 @@
+﻿using NServiceBus;
+using NServiceBus.TransactionalSession.AcceptanceTests;
+using NUnit.Framework;
+
+[SetUpFixture]
+public class MongoSetup
+{
+    [OneTimeSetUp]
+    public void Setup()
+    {
+        TransactionSessionDefaultServer.ConfigurePersistence = configuration =>
+        {
+            var persistence = configuration.UsePersistence<MongoPersistence>();
+            persistence.DatabaseName("TransactionalSessionAcceptanceTests");
+
+            //TODO: use transactions?
+        };
+    }
+}
