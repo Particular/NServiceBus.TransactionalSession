97b2a821c45441c9ca44dac9d0c16623f43de59e
Tim Bussmann <timbussmann@users.noreply.github.com>
2022-08-03 18:45:46 +0200

    update sqlp att

--- src/NServiceBus.TransactionalSession.AcceptanceTests.SqlP/When_not_using_outbox.cs
+++ src/NServiceBus.TransactionalSession.AcceptanceTests.SqlP/When_not_using_outbox.cs
@@ -1,13 +1,12 @@
 ﻿namespace NServiceBus.TransactionalSession.AcceptanceTests
 {
     using System;
-    using System.Threading;
     using System.Threading.Tasks;
     using Microsoft.Extensions.DependencyInjection;
     using AcceptanceTesting;
     using NUnit.Framework;
 
-    public class When_using_outbox : NServiceBusAcceptanceTest
+    public class When_not_using_outbox : NServiceBusAcceptanceTest
     {
         [Test]
         public async Task Should_send_messages_on_transactional_session_commit()
@@ -17,11 +16,12 @@
                 {
                     using var scope = ctx.ServiceProvider.CreateScope();
                     using var transactionalSession = scope.ServiceProvider.GetRequiredService<ITransactionalSession>();
+
                     await transactionalSession.OpenSqlSession();
 
-                    await transactionalSession.SendLocal(new SampleMessage(), CancellationToken.None);
+                    await transactionalSession.SendLocal(new SampleMessage());
 
-                    await transactionalSession.Commit(CancellationToken.None).ConfigureAwait(false);
+                    await transactionalSession.Commit();
                 }))
                 .Done(c => c.MessageReceived)
                 .Run();
@@ -31,7 +31,7 @@
         public async Task Should_not_send_messages_if_session_is_not_committed()
         {
             var result = await Scenario.Define<Context>()
-                .WithEndpoint<AnEndpoint>(s => s.When(async (statelessSession, ctx) =>
+                .WithEndpoint<AnEndpoint>(s => s.When(async (messageSession, ctx) =>
                 {
                     using (var scope = ctx.ServiceProvider.CreateScope())
                     using (var transactionalSession = scope.ServiceProvider.GetRequiredService<ITransactionalSession>())
@@ -42,36 +42,14 @@
                     }
 
                     //Send immediately dispatched message to finish the test
-                    await statelessSession.SendLocal(new CompleteTestMessage());
+                    await messageSession.SendLocal(new CompleteTestMessage());
                 }))
                 .Done(c => c.CompleteMessageReceived)
-                .Run();
-
-            Assert.True(result.CompleteMessageReceived);
-            Assert.False(result.MessageReceived);
-        }
-
-        [Test]
-        public async Task Should_send_immediate_dispatch_messages_even_if_session_is_not_committed()
-        {
-            var result = await Scenario.Define<Context>()
-                .WithEndpoint<AnEndpoint>(s => s.When(async (_, ctx) =>
-                {
-                    using var scope = ctx.ServiceProvider.CreateScope();
-                    using var transactionalSession = scope.ServiceProvider.GetRequiredService<ITransactionalSession>();
-
-                    await transactionalSession.OpenSqlSession();
-
-                    var sendOptions = new SendOptions();
-                    sendOptions.RequireImmediateDispatch();
-                    sendOptions.RouteToThisEndpoint();
-                    await transactionalSession.Send(new SampleMessage(), sendOptions, CancellationToken.None);
-                }))
-                .Done(c => c.MessageReceived)
                 .Run()
                 ;
 
-            Assert.True(result.MessageReceived);
+            Assert.True(result.CompleteMessageReceived);
+            Assert.False(result.MessageReceived);
         }
 
         class Context : ScenarioContext, IInjectServiceProvider
@@ -83,8 +61,7 @@
 
         class AnEndpoint : EndpointConfigurationBuilder
         {
-            public AnEndpoint() =>
-                EndpointSetup<TransactionSessionWithOutboxEndpoint>();
+            public AnEndpoint() => EndpointSetup<TransactionSessionDefaultServer>();
 
             class SampleHandler : IHandleMessages<SampleMessage>
             {
@@ -102,7 +79,8 @@
 
             class CompleteTestMessageHandler : IHandleMessages<CompleteTestMessage>
             {
-                public CompleteTestMessageHandler(Context context) => testContext = context;
+
+                public CompleteTestMessageHandler(Context testContext) => this.testContext = testContext;
 
                 public Task Handle(CompleteTestMessage message, IMessageHandlerContext context)
                 {
