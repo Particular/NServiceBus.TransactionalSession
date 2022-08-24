93928493e3af4a9a26bf3d958bee37142edc06a0
Daniel Marbach <daniel.marbach@openplace.net>
2022-08-24 10:30:50 +0200

    Add non outboxed test for CosmosDB

---
+++ src/NServiceBus.TransactionalSession.AcceptanceTests.CosmosDB/When_not_using_outbox.cs
@@ -0,0 +1,117 @@
+﻿namespace NServiceBus.TransactionalSession.AcceptanceTests
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.DependencyInjection;
+    using AcceptanceTesting;
+    using NUnit.Framework;
+
+    [ExecuteOnlyForEnvironmentWith(EnvironmentVariables.CosmosConnectionString)]
+    public class When_not_using_outbox : NServiceBusAcceptanceTest
+    {
+        static string PartitionKeyHeaderName = "Tests.PartitionKey";
+        static string PartitionKeyValue = "SomePartition";
+
+        [Test]
+        public async Task Should_send_messages_on_transactional_session_commit()
+        {
+            await Scenario.Define<Context>()
+                .WithEndpoint<AnEndpoint>(s => s.When(async (_, ctx) =>
+                {
+                    using var scope = ctx.ServiceProvider.CreateScope();
+                    using var transactionalSession = scope.ServiceProvider.GetRequiredService<ITransactionalSession>();
+
+                    await transactionalSession.OpenCosmosDBSession(PartitionKeyValue);
+
+                    var sendOptions = new SendOptions();
+                    sendOptions.SetHeader(PartitionKeyHeaderName, PartitionKeyValue);
+                    sendOptions.RequireImmediateDispatch();
+                    sendOptions.RouteToThisEndpoint();
+                    await transactionalSession.Send(new SampleMessage(), sendOptions, CancellationToken.None);
+
+                    await transactionalSession.Commit();
+                }))
+                .Done(c => c.MessageReceived)
+                .Run();
+        }
+
+        [Test]
+        public async Task Should_not_send_messages_if_session_is_not_committed()
+        {
+            var result = await Scenario.Define<Context>()
+                .WithEndpoint<AnEndpoint>(s => s.When(async (messageSession, ctx) =>
+                {
+                    using (var scope = ctx.ServiceProvider.CreateScope())
+                    using (var transactionalSession = scope.ServiceProvider.GetRequiredService<ITransactionalSession>())
+                    {
+                        await transactionalSession.OpenCosmosDBSession(PartitionKeyValue);
+
+                        await transactionalSession.SendLocal(new SampleMessage());
+                    }
+
+                    //Send immediately dispatched message to finish the test
+                    await messageSession.SendLocal(new CompleteTestMessage());
+                }))
+                .Done(c => c.CompleteMessageReceived)
+                .Run();
+
+            Assert.True(result.CompleteMessageReceived);
+            Assert.False(result.MessageReceived);
+        }
+
+        class Context : ScenarioContext, IInjectServiceProvider
+        {
+            public bool MessageReceived { get; set; }
+            public bool CompleteMessageReceived { get; set; }
+            public IServiceProvider ServiceProvider { get; set; }
+        }
+
+        class AnEndpoint : EndpointConfigurationBuilder
+        {
+            public AnEndpoint() => EndpointSetup<TransactionSessionDefaultServer>(c =>
+            {
+                var persistence = c.UsePersistence<CosmosPersistence>();
+
+                persistence.TransactionInformation().ExtractPartitionKeyFromHeader(PartitionKeyHeaderName);
+            });
+
+            class SampleHandler : IHandleMessages<SampleMessage>
+            {
+                public SampleHandler(Context testContext) => this.testContext = testContext;
+
+                public Task Handle(SampleMessage message, IMessageHandlerContext context)
+                {
+                    testContext.MessageReceived = true;
+
+                    return Task.CompletedTask;
+                }
+
+                readonly Context testContext;
+            }
+
+            class CompleteTestMessageHandler : IHandleMessages<CompleteTestMessage>
+            {
+
+                public CompleteTestMessageHandler(Context testContext) => this.testContext = testContext;
+
+                public Task Handle(CompleteTestMessage message, IMessageHandlerContext context)
+                {
+                    testContext.CompleteMessageReceived = true;
+
+                    return Task.CompletedTask;
+                }
+
+                readonly Context testContext;
+            }
+        }
+
+        class SampleMessage : ICommand
+        {
+        }
+
+        class CompleteTestMessage : ICommand
+        {
+        }
+    }
+}
