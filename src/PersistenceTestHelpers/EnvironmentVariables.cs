22f4f93c2d88aeadab49a31ddf65aa141442b07e
Tim Bussmann <timbussmann@users.noreply.github.com>
2022-08-30 15:46:51 +0200

    add test helpers project

---
+++ src/PersistenceTestHelpers/EnvironmentVariables.cs
@@ -0,0 +1,12 @@
+﻿public static class EnvironmentVariables
+{
+    public const string AzureTableServerConnectionString = "AzureTableServerConnectionString";
+
+    public const string RavenDBConnectionString = "RavenDBConnectionString";
+
+    public const string SqlServerConnectionString = "SQLServerConnectionString";
+
+    public const string CosmosConnectionString = "CosmosDBPersistence_ConnectionString";
+
+    public const string MongoDBConnectionString = "MongoDBConnectionString";
+}
