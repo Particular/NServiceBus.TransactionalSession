54753be30f78636a206d60156ebfd8f22ed949be
Tim Bussmann <timbussmann@users.noreply.github.com>
2022-08-30 15:47:05 +0200

    add mongodb acceptance test

---
+++ src/NServiceBus.TransactionalSession.AcceptanceTests.MongoDB/MongoSetup.cs
@@ -0,0 +1,45 @@
+﻿namespace NServiceBus.TransactionalSession.AcceptanceTests
+{
+    using System;
+    using System.Threading.Tasks;
+    using MongoDB.Driver;
+    using NServiceBus;
+    using NUnit.Framework;
+
+    [SetUpFixture]
+    [ExecuteOnlyForEnvironmentWith(EnvironmentVariables.MongoDBConnectionString)]
+    public class MongoSetup
+    {
+        public const string DatabaseName = "TransactionalSessionAcceptanceTests";
+
+        public static MongoClient MongoClient { get; private set; }
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            var containerConnectionString = EnvironmentHelper.GetEnvironmentVariable(EnvironmentVariables.MongoDBConnectionString);
+            MongoClient = string.IsNullOrWhiteSpace(containerConnectionString) ? new MongoClient() : new MongoClient(containerConnectionString);
+
+            TransactionSessionDefaultServer.ConfigurePersistence = configuration =>
+            {
+                var persistence = configuration.UsePersistence<MongoPersistence>();
+                persistence.MongoClient(MongoClient);
+                persistence.DatabaseName(DatabaseName);
+                persistence.UseTransactions(true);
+            };
+        }
+
+        [OneTimeTearDown]
+        public async Task Cleanup()
+        {
+            try
+            {
+                await MongoClient.DropDatabaseAsync(DatabaseName);
+            }
+            catch (Exception e)
+            {
+                TestContext.WriteLine($"Error during MongoDB test cleanup: {e}");
+            }
+        }
+    }
+}
