68abb7ff3ff659bdba1311493b98f197ac81a929
lailabougria <laila.bougria@particular.net>
2022-09-02 11:59:51 +0200

    Introduce customizations extension point
    Co-authored-by: Daniel Marbach <daniel.marbach@particular.net>
    Co-authored-by: Tim Bussmann <tim.bussmann@particular.net>

---
+++ src/NServiceBus.TransactionalSession/IOpenSessionOptionsCustomization.cs
@@ -0,0 +1,13 @@
+﻿namespace NServiceBus.TransactionalSession;
+
+/// <summary>
+/// An extension point that allows for customization of settings when opening the <see cref="ITransactionalSession"/>.
+/// </summary>
+public interface IOpenSessionOptionsCustomization
+{
+    /// <summary>
+    /// Applies custom settings to the specified <paramref name="options"/>
+    /// </summary>
+    /// <param name="options">The options to which the customizations are applied</param>
+    void Apply(OpenSessionOptions options);
+}
