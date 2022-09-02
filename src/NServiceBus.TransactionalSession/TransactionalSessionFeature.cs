967c264338970516cad9838e3236445534838d7c
lailabougria <laila.bougria@particular.net>
2022-09-02 11:24:44 +0200

    make it build

--- src/NServiceBus.TransactionalSession/TransactionalSessionFeature.cs
+++ src/NServiceBus.TransactionalSession/TransactionalSessionFeature.cs
@@ -1,6 +1,5 @@
 ﻿namespace NServiceBus.TransactionalSession
 {
-    using System;
     using System.Threading;
     using System.Threading.Tasks;
     using Features;
@@ -20,18 +19,6 @@
         protected override void Setup(FeatureConfigurationContext context)
         {
             QueueAddress localQueueAddress = context.LocalQueueAddress();
-
-            // Due to ordering issues it might not always be possible to check whether the feature is active. Checking for enabled and active is the safest bet since the behaviors use defensive techniques to get the values
-            if (context.Settings.TryGet("NServiceBus.Persistence.CosmosDB.SynchronizedStorage", out FeatureState cosmosSate) && cosmosSate is FeatureState.Enabled or FeatureState.Active)
-            {
-                context.Pipeline.Register(new CosmosDBSupport.CosmosControlMessageBehavior(), "Propagates control message header values to PartitionKeys and ContainerInformation when necessary.");
-            }
-
-            // Due to ordering issues it might not always be possible to check whether the feature is active. Checking for enabled and active is the safest bet since the behaviors use defensive techniques to get the values
-            if (context.Settings.TryGet("NServiceBus.Persistence.AzureTable.SynchronizedStorage", out FeatureState azureTableState) && azureTableState is FeatureState.Enabled or FeatureState.Active)
-            {
-                context.Pipeline.Register(new AzureTableSupport.AzureTableControlMessageBehavior(), "Propagates control message header values to TableEntityPartitionKeys and TableInformation when necessary.");
-            }
 
             var isOutboxEnabled = context.Settings.IsFeatureActive(typeof(Outbox));
             var sessionCaptureTask = new SessionCaptureTask();
@@ -59,16 +46,6 @@
                         sp.GetRequiredService<ICompletableSynchronizedStorageSession>(),
                         sessionCaptureTask.CapturedSession,
                         sp.GetRequiredService<IMessageDispatcher>());
-                }
-
-                if (context.Settings.TryGet("NServiceBus.Features.NHibernateOutbox", out FeatureState nhState) && nhState == FeatureState.Active)
-                {
-                    transactionalSession.PersisterSpecificOptions.Set(context.Settings.EndpointName());
-                }
-
-                if (context.Settings.TryGet("NServiceBus.Persistence.AzureTable.SynchronizedStorage", out FeatureState atState) && atState == FeatureState.Active)
-                {
-                    transactionalSession.PersisterSpecificOptions.Set(sp.GetRequiredService(Type.GetType(AzureTableSupport.TableHolderResolverAssemblyQualifiedTypeName)));
                 }
 
                 return transactionalSession;
