c1f87c2bed943cd646320569ea09fa2c14025da1
Tim Bussmann <timbussmann@users.noreply.github.com>
2023-04-28 14:55:38 +0200

    Complete synchronized storage session before storing outbox data (#191)
    * add test
    
    * complete session before storing outbox operations
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Daniel Marbach <daniel.marbach@openplace.net>

--- src/NServiceBus.TransactionalSession.Tests/OutboxTransactionalSessionTests.cs
+++ src/NServiceBus.TransactionalSession.Tests/OutboxTransactionalSessionTests.cs
@@ -169,8 +169,25 @@
             Assert.AreEqual(bool.TrueString, controlMessage.Message.Headers[Headers.ControlMessageHeader]);
 
             var outboxTransaction = outboxStorage.StartedTransactions.Single();
-            Assert.IsFalse(completableSynchronizedStorageSession.Completed, "should not have completed synchronized storage session");
-            Assert.IsFalse(outboxTransaction.Commited, "should not have commited outbox operations");
+            Assert.IsFalse(outboxTransaction.Commited, "should not have committed outbox operations");
+        }
+
+        [Test]
+        public async Task Commit_should_complete_synchronized_storage_session_before_outbox_store()
+        {
+            var messageSession = new FakeMessageSession();
+            var dispatcher = new FakeDispatcher();
+            var outboxStorage = new FakeOutboxStorage { StoreCallback = (_, _, _) => throw new Exception("some error") };
+            var completableSynchronizedStorageSession = new FakeSynchronizableStorageSession();
+            using var session = new OutboxTransactionalSession(outboxStorage, completableSynchronizedStorageSession, messageSession, dispatcher, Enumerable.Empty<IOpenSessionOptionsCustomization>(), "queue address");
+
+            await session.Open(new FakeOpenSessionOptions());
+            await session.Send(new object());
+            Assert.ThrowsAsync<Exception>(async () => await session.Commit());
+
+            var outboxTransaction = outboxStorage.StartedTransactions.Single();
+            Assert.IsTrue(completableSynchronizedStorageSession.Completed, "should have completed synchronized storage session to match the receive pipeline behavior");
+            Assert.IsFalse(outboxTransaction.Commited, "should not have committed outbox operations");
         }
 
         [Test]
