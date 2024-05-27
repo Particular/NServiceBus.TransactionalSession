735ab36194a2e3edeceff4fa3ec3fbc0ad8731ce
Daniel Marbach <danielmarbach@users.noreply.github.com>
2024-05-27 15:39:56 +0200

    Make the commit delay increment overridable

--- src/NServiceBus.TransactionalSession/OpenSessionOptions.cs
+++ src/NServiceBus.TransactionalSession/OpenSessionOptions.cs
@@ -31,9 +31,14 @@ namespace NServiceBus.TransactionalSession
         /// The maximum duration the transaction is allowed to attempt to atomically commit.
         /// </summary>
         /// <remarks>The actual total transaction time observed might be longer, taking into account delays in the transport due to latency, delayed delivery and more.</remarks>
+        /// <remarks>Defaults to <code>TimeSpan.FromSeconds(15)</code></remarks>
         public TimeSpan MaximumCommitDuration { get; set; } = TimeSpan.FromSeconds(15);
 
-        internal TimeSpan CommitDelayIncrement { get; set; } = TimeSpan.FromSeconds(2);
+        /// <summary>
+        /// The time increment used to delay the commit of the transactional session when the outbox record is not yet in the storage.
+        /// </summary>
+        /// <remarks>Defaults to <code>TimeSpan.FromSeconds(2)</code></remarks>
+        public TimeSpan CommitDelayIncrement { get; set; } = TimeSpan.FromSeconds(2);
 
         Dictionary<string, string> metadata;
     }
