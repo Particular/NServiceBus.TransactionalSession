7613c8c36a21fda70a09c191557dfefc91487c69
Andreas Öhlund <andreas.ohlund@particular.net>
2025-04-04 09:43:31 +0200

    Support send-only endpoints for non-outbox mode (#425)
    * Support send-only endpoints for non-outbox mode
    
    * Separate test class
    
    * Cleanup
    
    * Better names
    
    * Align name

---
+++ src/NServiceBus.TransactionalSession.AcceptanceTests/When_not_using_outbox_send_only.cs
@@ -0,0 +1,67 @@
+﻿namespace NServiceBus.TransactionalSession.AcceptanceTests;
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using AcceptanceTesting;
+using AcceptanceTesting.Customization;
+using NUnit.Framework;
+
+public class When_not_using_outbox_send_only : NServiceBusAcceptanceTest
+{
+    [Test]
+    public async Task Should_send_messages_on_transactional_session_commit()
+    {
+        var result = await Scenario.Define<Context>()
+            .WithEndpoint<SendOnlyEndpoint>(s => s.When(async (_, ctx) =>
+            {
+                using var scope = ctx.ServiceProvider.CreateScope();
+                using var transactionalSession = scope.ServiceProvider.GetRequiredService<ITransactionalSession>();
+
+                await transactionalSession.Open(new CustomTestingPersistenceOpenSessionOptions());
+
+                var options = new SendOptions();
+
+                options.SetDestination(Conventions.EndpointNamingConvention.Invoke(typeof(AnotherEndpoint)));
+
+                await transactionalSession.Send(new SampleMessage(), options);
+
+                await transactionalSession.Commit();
+            }))
+            .WithEndpoint<AnotherEndpoint>()
+            .Done(c => c.MessageReceived)
+            .Run();
+
+        Assert.That(result.MessageReceived, Is.True);
+    }
+
+    class Context : ScenarioContext, IInjectServiceProvider
+    {
+        public bool MessageReceived { get; set; }
+        public IServiceProvider ServiceProvider { get; set; }
+    }
+
+    class SendOnlyEndpoint : EndpointConfigurationBuilder
+    {
+        public SendOnlyEndpoint() => EndpointSetup<TransactionSessionDefaultServer>(c => c.SendOnly());
+    }
+
+    class AnotherEndpoint : EndpointConfigurationBuilder
+    {
+        public AnotherEndpoint() => EndpointSetup<TransactionSessionDefaultServer>();
+
+        class SampleHandler(Context testContext) : IHandleMessages<SampleMessage>
+        {
+            public Task Handle(SampleMessage message, IMessageHandlerContext context)
+            {
+                testContext.MessageReceived = true;
+
+                return Task.CompletedTask;
+            }
+        }
+    }
+
+    class SampleMessage : ICommand
+    {
+    }
+}
